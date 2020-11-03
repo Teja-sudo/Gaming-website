8de02ff36ed5b6d0b2791503e1c8244b4a0d9c27
Sanikommu Teja <sanikommutejareddy@gmail.com>
2020-11-03 06:58:22 +0530

    fourth commit

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ function hamburgerMenu(){
     navMenu.style.display = navMenu.offsetParent === null ? 'block' : 'none';
 }
 
+$('#nav-menu-container a').on('click', function (e) {
+       if (this.hash !== '') {
+         e.preventDefault();
+    
+         const hash = this.hash;
+    
+         $('html, body')
+           .animate({
+             scrollTop: $(hash).offset().top - 80
+           },800);
+       }
+     });
